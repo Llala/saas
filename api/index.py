30da364b75bb5fc0bf4d156fa462e0ed662bc910
Evgeniya Yuzhikina <evgeniya.yuzhikina@gmail.com>
2026-07-26 13:18:57 +0300

    Fix FastAPI routes

--- api/index.py
+++ api/index.py
@@ -1,27 +1,35 @@
-from fastapi import FastAPI  # type: ignore
-from fastapi.responses import PlainTextResponse  # type: ignore
-from openai import OpenAI  # type: ignore
-import os
+# from fastapi import FastAPI  # type: ignore
+# from fastapi.responses import PlainTextResponse  # type: ignore
+# from openai import OpenAI  # type: ignore
+# import os
+
+# app = FastAPI()
+
+# # @app.get("/api/debug")
+# # def debug():
+# #     return {
+# #         "has_key": "OPENAI_API_KEY" in os.environ,
+# #         "prefix": os.environ.get("OPENAI_API_KEY", "")[:8],
+# #     }
+
+# # @app.get("/api", response_class=PlainTextResponse)
+# # def idea():
+# #     client = OpenAI()
+# #     prompt = [{"role": "user", "content": "Come up with a new business idea for AI Agents"}]
+# #     response = client.chat.completions.create(model="gpt-5-nano", messages=prompt)
+# #     return response.choices[0].message.content
+
+# @app.get("/")
+# def root():
+#     return {
+#         "version": "2026-07-26-test",
+#         "routes": ["/", "/debug"]
+#     }
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
-# @app.get("/debug")
-# def debug():
-#     return {
-#         "has_key": "OPENAI_API_KEY" in os.environ,
-#         "prefix": os.environ.get("OPENAI_API_KEY", "")[:8],
-#     }
-
-# @app.get("/", response_class=PlainTextResponse)
-# def idea():
-#     client = OpenAI()
-#     prompt = [{"role": "user", "content": "Come up with a new business idea for AI Agents"}]
-#     response = client.chat.completions.create(model="gpt-5-nano", messages=prompt)
-#     return response.choices[0].message.content
-
-@app.get("/")
+@app.get("/фзш")
 def root():
-    return {
-        "version": "2026-07-26-test",
-        "routes": ["/", "/debug"]
-    }
+    return {"version": "DEBUG-12345"}
